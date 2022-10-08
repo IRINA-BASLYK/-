eb2385adc003b44342c82692ad8ed953f7171cc2
Irina_Baslyk <ira.baslyk@mail.ru>
2022-10-08 23:55:16 +0300

    дз_10

---
+++ дз_10/Program.cs
@@ -0,0 +1,19 @@
+﻿/* // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+while (num < 100 || num > 1000)
+{
+    Console.WriteLine("Введите трехзначное число");
+    num = int.Parse(Console.ReadLine());
+}
+
+int x = num % 10;
+int result = (num - x) % 100 / 10;
+Console.WriteLine("вторая цифра этого числа -> " + result);
+
+
